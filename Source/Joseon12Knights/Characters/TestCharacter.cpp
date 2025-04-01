1c4f0e03be799591f2a0c5d8dcd1092b1d062923
robertobbaik <robertobbaik@gmail.com>
2025-04-01 22:54:25 +0900

    [Add] PlayerCharacter / TestPlayerCharacter, IA, IMC

---
+++ Source/Joseon12Knights/Characters/TestCharacter.cpp
@@ -0,0 +1,2 @@
+#include "TestCharacter.h"
+
---
+++ Source/Joseon12Knights/Characters/TestCharacter.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PlayerCharacter.h"
+#include "TestCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JOSEON12KNIGHTS_API ATestCharacter : public APlayerCharacter
+{
+	GENERATED_BODY()
+	
+};
