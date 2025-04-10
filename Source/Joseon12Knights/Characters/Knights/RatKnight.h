d5748b47d0c00bf082028f9c48accc2a42be6264
raindrovvv <vnrs97@gmail.com>
2025-04-10 15:30:55 +0900

    ✨Update : 자 기사 스킬 구현(W,R)
    - 은신 스킬 구현
    - 궁극기 구현
    - TODO : 타겟팅 로직 구현 예정

--- Source/Joseon12Knights/Characters/Knights/RatKnight.h
+++ Source/Joseon12Knights/Characters/Knights/RatKnight.h
@@ -1,10 +1,14 @@
-
-
+﻿
 #pragma once
 
 #include "CoreMinimal.h"
 #include "PlayerCharacter.h"
+#include "Materials/MaterialInstanceDynamic.h"
 #include "RatKnight.generated.h"
+
+// 자 기사의 스킬은 모두 공격형, 디버프 효과로 적용됨.
+// 1) W : 쥐구멍 – 슬로우 디버프 & 은신
+// 2) R : 1등 난타 – 독 디버프
 
 UCLASS(Blueprintable)
 class JOSEON12KNIGHTS_API ARatKnight : public APlayerCharacter
@@ -12,32 +16,70 @@ class JOSEON12KNIGHTS_API ARatKnight : public APlayerCharacter
 	GENERATED_BODY()
 
 protected:
-	
-	// WŰ ��ų : �㱸��
-	void Skill(const FInputActionValue& Value) override;
-	// RŰ �ñر� : 1�� ��Ÿ
-	void Ultimate(const FInputActionValue& Value) override;
+	void Skill(const FInputActionValue& Value) override; // W키 스킬 : 쥐구멍
+	void Ultimate(const FInputActionValue& Value) override; // R키 궁극기 : 1등 난타
 
-	// W ��ų ����
-
+	// ===========
+	// W Skill Asset
+	// ===========
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "RatKnight Skill")
 	UParticleSystem* RatSkill1Effect;
-
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "RatKnight Skill")
 	USoundBase* RatSkill1Sound;
 
-	// R �ñر� ����
+	// 은신 관련 효과
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "RatKnight Skill")
+	UParticleSystem* StealthEndEffect;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "RatKnight Skill")
+	USoundBase* StealthEndSound;
 
+	// 은신 머티리얼 관련
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "RatKnight Stealth")
+	TArray<UMaterialInterface*> OriginalMaterials;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "RatKnight Stealth")
+	TArray<UMaterialInterface*> StealthMaterials;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "RatKnight Stealth")
+	TArray<UMaterialInterface*> OriginalWeaponMaterials;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "RatKnight Stealth")
+	TArray<UMaterialInterface*> OriginalShieldMaterials;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "RatKnight Stealth")
+	TArray<UMaterialInterface*> StealthWeaponMaterials;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "RatKnight Stealth")
+	TArray<UMaterialInterface*> StealthShieldMaterials;
+
+	// ===========
+	// R Skill Asset
+	// ===========
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "RatKnight Skill")
 	UParticleSystem* RatUltimateEffect;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "RatKnight Skill")
 	USoundBase* RatUltimateSound;
 
+public:
+	virtual void BeginPlay() override;
+
+	// 은신 관련 함수
+	void SetStealthVisibility(bool bIsStealthed);
+	void EndStealth();
+
 private:
-	// ��Ÿ�� ���� ����
+	// 쿨타임 관리 변수
 	bool bCanUseSkill = true;
 	bool bCanUseUltimate = true;
 	FTimerHandle SkillCooldownTimerHandle;
 	FTimerHandle UltimateCooldownTimerHandle;
+
+	// 은신 관련 변수
+	bool bIsInStealthMode = false;
+	FTimerHandle StealthTimerHandle;
+	float StealthDuration = 5.0f;
+
+	// 임시 타게팅 함수 (로직 구현 필요)
+	APlayerCharacter* GetTargetPlayer();
 };
