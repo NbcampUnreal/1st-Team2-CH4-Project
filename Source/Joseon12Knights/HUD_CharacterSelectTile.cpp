71d672580b6a135c1699eb9867c6909ce8805f1f
Joel970203 <rngkfk0203@naver.com>
2025-04-10 15:37:06 +0900

    edit bp

--- Source/Joseon12Knights/HUD_CharacterSelectTile.cpp
+++ Source/Joseon12Knights/HUD_CharacterSelectTile.cpp
@@ -21,7 +21,6 @@ void UHUD_CharacterSelectTile::NativeConstruct()
 void UHUD_CharacterSelectTile::SetupCharacterTile(const FString& InCharacterID)
 {
     CharacterID = InCharacterID;
-
     CharacterNameMap = 
     {
         {TEXT("1"), TEXT("자 기사")},
@@ -29,9 +28,9 @@ void UHUD_CharacterSelectTile::SetupCharacterTile(const FString& InCharacterID)
         {TEXT("3"), TEXT("인 기사")},
         {TEXT("4"), TEXT("진 기사")},
         {TEXT("5"), TEXT("잠금")},
-        {TEXT("6"), TEXT("잠금2")},
-        {TEXT("7"), TEXT("잠금3")},
-        {TEXT("8"), TEXT("잠금4")}
+        {TEXT("6"), TEXT("잠금")},
+        {TEXT("7"), TEXT("잠금")},
+        {TEXT("8"), TEXT("잠금")}
     };
 
     FString* DisplayName = CharacterNameMap.Find(InCharacterID);
