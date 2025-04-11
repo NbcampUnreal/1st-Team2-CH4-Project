cd4ec013575da923e82cb2a6a14cd95ecbae89cd
raindrovvv <vnrs97@gmail.com>
2025-04-11 20:05:17 +0900

    ✨Update : TigerKnight 도트 데미지 기능 구현, 디버그 임시 구현

--- Source/Joseon12Knights/Characters/CharacterComponent/BuffComponent.cpp
+++ Source/Joseon12Knights/Characters/CharacterComponent/BuffComponent.cpp
@@ -70,6 +70,7 @@ float UBuffComponent::GetAttackMultiplier() const
 	{
 		Multiplier *= Buff.AttackMultiplier;
 	}
+	UE_LOG(LogTemp, Warning, TEXT("Attack Multiplier: %.2f"), Multiplier);
 	return Multiplier;
 }
 
@@ -80,6 +81,7 @@ float UBuffComponent::GetDefenseMultiplier() const
 	{
 		Multiplier *= Buff.DefenseMultiplier;
 	}
+	UE_LOG(LogTemp, Warning, TEXT("Defense Multiplier: %.2f"), Multiplier);
 	return Multiplier;
 }
 
@@ -90,6 +92,7 @@ float UBuffComponent::GetMoveSpeedMultiplier() const
 	{
 		Multiplier *= Buff.MoveSpeedMultiplier;
 	}
+	UE_LOG(LogTemp, Warning, TEXT("Move Speed Multiplier: %.2f"), Multiplier);
 	return Multiplier;
 }
 
--- Source/Joseon12Knights/Characters/Knights/TigerKnight.h
+++ Source/Joseon12Knights/Characters/Knights/TigerKnight.h
@@ -12,6 +12,7 @@ protected:
 	void Skill(const FInputActionValue& Value) override;
 	void Ultimate(const FInputActionValue& Value) override;
 
+	// W 스킬 에셋
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TigerKnight Skill")
 	UParticleSystem* TigerSkill1Effect;
 
@@ -19,14 +20,11 @@ protected:
 	USoundBase* TigerSkill1Sound;
 
 	// R 궁극기 에셋
-
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TigerKnight Skill")
 	UParticleSystem* TigerUltimateEffect;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TigerKnight Skill")
 	USoundBase* TigerUltimateSound;
-
-
 
 private:
 	// 쿨타임 관리 변수
@@ -35,20 +33,11 @@ private:
 	FTimerHandle SkillCooldownTimerHandle;
 	FTimerHandle UltimateCooldownTimerHandle;
 
-	//APlayerCharacter* GetTargetPlayer();
-	//TArray<APlayerCharacter*> FindTargetsInRadius(const FVector& Origin, float Radius);
-	//APlayerCharacter* SelectBestTarget(const TArray<APlayerCharacter*>& PotentialTargets);
-	//
 
-	//UPROPERTY(EditAnywhere, Category = "Targeting")
-	//float AttackRadius = 106.0f;  // 공격 감지 반경
-
-	//UPROPERTY(EditAnywhere, Category = "Targeting")
-	//float ForwardOffset = 30.0f; // 전방 오프셋 거리
-
-	//UPROPERTY(EditAnywhere, Category = "Targeting")
-	//bool bDebugTargeting = false; // 디버그 시각화 여부
-
-	//UPROPERTY(EditAnywhere, Category = "Targeting")
-	//TEnumAsByte<ECollisionChannel> TargetCollisionChannel = ECC_Pawn; // 타겟 감지 채널
+// ==========
+//  디버그 함수
+// ==========
+public:
+	UFUNCTION(BlueprintCallable, Category = "Debug")
+	void DebugPrintBuffs() const;
 };
